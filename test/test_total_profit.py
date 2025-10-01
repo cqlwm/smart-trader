21f2280b3f4647852d7455541a24b383c698876a
li <weimin.li.dever@gmail.com>
2025-10-01 08:35:57 +0800

    -

--- test/test_total_profit.py
+++ test/test_total_profit.py
@@ -1,7 +1,11 @@
 import json
+import sys
+
+# 从启动参数中获取file路径
+file_path = sys.argv[1]
 
 # 读取JSON文件内容
-with open('data/backup_dogeusdc_short_sell.json', 'r') as file:
+with open(file_path, 'r') as file:
     data = json.load(file)
 
 # 初始化总收益
