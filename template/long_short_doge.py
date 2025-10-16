5114bc7248eeae047e8b3f3c1af1ec3110864351
li <weimin.li.dever@gmail.com>
2025-10-16 21:15:01 +0800

    -

--- template/long_short_doge.py
+++ template/long_short_doge.py
@@ -9,6 +9,30 @@ from task.strategy_task import StrategyTask
 
 logger = log.getLogger(__name__)
 
+def template_short_sell(exchange_client: ExSwapClient) -> StrategyTask:
+    symbol=Symbol(base="doge", quote="usdt")
+    timeframe='1m'
+
+    config=SignalGridStrategyConfig(
+        symbol=symbol,
+        position_side=PositionSide.SHORT,
+        master_side=OrderSide.SELL,
+        per_order_qty=42,
+        grid_spacing_rate=0.0002,
+        max_order=10,
+        # highest_price=1,
+        # lowest_price=0,
+        enable_exit_signal=True,
+        signal=AlphaTrendGridsSignal(AlphaTrendSignal(OrderSide.BUY)),
+        signal_min_take_profit_rate=0.004,
+        enable_fixed_profit_taking=True,
+        fixed_take_profit_rate=0.01,
+        order_file_path=f'{DATA_PATH}/signal_grid_short_sell_{symbol.simple()}_{timeframe}.json',
+    )
+    strategy = SignalGridStrategy(config, exchange_client)
+
+    return StrategyTask(symbol=symbol, timeframe=timeframe, strategy=strategy)
+
 def template_long(exchange_client: ExSwapClient) -> StrategyTask:
     symbol=Symbol(base="doge", quote="usdt")
     timeframe='1m'
