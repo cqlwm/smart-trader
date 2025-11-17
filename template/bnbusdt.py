0a913cffad1b1a4e0fb37e6291bc2ec859adf296
li <weimin.li.dever@gmail.com>
2025-11-17 20:50:03 +0800

    -

--- template/bnbusdt.py
+++ template/bnbusdt.py
@@ -5,6 +5,7 @@ from strategy.grids_strategy_v2 import SignalGridStrategy, SignalGridStrategyCon
 from strategy.alpha_trend_signal.alpha_trend_signal import AlphaTrendSignal
 from strategy.alpha_trend_signal.alpha_trend_grids_signal import AlphaTrendGridsSignal
 from config import DATA_PATH
+from strategy.simple_grid_strategy_v2 import SimpleGridStrategy, SimpleGridStrategyConfig
 from task.strategy_task import StrategyTask
 
 logger = log.getLogger(__name__)
@@ -58,3 +59,32 @@ def short_sell(exchange_client: ExSwapClient) -> StrategyTask:
     strategy = SignalGridStrategy(config, exchange_client)
 
     return StrategyTask(symbol=symbol, timeframe=timeframe, strategy=strategy)
+
+
+
+def simple_grid_long_buy(exchange_client: ExSwapClient) -> StrategyTask:
+    """
+    简单网格策略模板
+    """
+    symbol=Symbol(base="bnb", quote="usdt")
+    timeframe='1m'
+    upper_price=1006.27
+    lower_price=891.51
+    grid_num=26
+
+    config = SimpleGridStrategyConfig(
+        symbol=symbol,
+        upper_price=upper_price,
+        lower_price=lower_price,
+        grid_num=grid_num,
+        quantity_per_grid=0.01,
+        position_side=PositionSide.LONG,
+        master_order_side=OrderSide.BUY,
+        active_grid_count=8,
+        delay_pending_order=True,
+        initial_quota=1,
+        backup_file=f'{DATA_PATH}/simple_grid_long_buy_{symbol.simple()}_{int(lower_price)}_{int(upper_price)}_{grid_num}.json',
+    )
+
+    simple_grid_strategy = SimpleGridStrategy(ex_client=exchange_client, config=config)
+    return StrategyTask(symbol=symbol, timeframe=timeframe, strategy=simple_grid_strategy)
