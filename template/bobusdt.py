63e4dde5e61e1d70c33fcc6feb52890a85380bb4
li <weimin.li.dever@gmail.com>
2025-11-30 08:48:17 +0800

    -

--- template/bobusdt.py
+++ template/bobusdt.py
@@ -18,17 +18,17 @@ def short_sell(exchange_client: ExSwapClient) -> StrategyTask:
         position_side=PositionSide.SHORT,
         master_side=OrderSide.SELL,
         per_order_qty=600,
-        grid_spacing_rate=-0.5,
+        grid_spacing_rate=0.0001,
         max_order=10,
         # enable_exit_signal=True,
         # signal=AlphaTrendGridsSignal(AlphaTrendSignal(OrderSide.BUY)),
         # signal_min_take_profit_rate=0.005,
         enable_fixed_profit_taking=True,
-        fixed_take_profit_rate=0.9,
+        fixed_take_profit_rate=0.0001,
         order_file_path=f'{DATA_PATH}/signal_grid_short_sell_{symbol.simple()}_{timeframe}.json',
         enable_max_order_stop_loss=True,
         paused_after_stop_loss=False,
-        place_order_behavior=PlaceOrderBehavior.CHASER,
+        # place_order_behavior=PlaceOrderBehavior.CHASER,
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
