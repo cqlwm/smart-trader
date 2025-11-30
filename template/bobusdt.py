b1ffd26c959098a089c3ef214ede017b35a39c5a
li <weimin.li.dever@gmail.com>
2025-11-30 10:24:50 +0800

    -

--- template/bobusdt.py
+++ template/bobusdt.py
@@ -41,17 +41,14 @@ def long_buy(exchange_client: ExSwapClient) -> StrategyTask:
         position_side=PositionSide.LONG,
         master_side=OrderSide.BUY,
         per_order_qty=600,
-        grid_spacing_rate=-0.5,
+        grid_spacing_rate=0.0001,
         max_order=10,
-        # enable_exit_signal=True,
-        # signal=AlphaTrendGridsSignal(AlphaTrendSignal(OrderSide.BUY)),
-        # signal_min_take_profit_rate=0.005,
         enable_fixed_profit_taking=True,
-        fixed_take_profit_rate=0.9,
+        fixed_take_profit_rate=0.0001,
         order_file_path=f'{DATA_PATH}/signal_grid_long_buy_{symbol.simple()}_{timeframe}.json',
+        enable_limit_take_profit=True,
         enable_max_order_stop_loss=True,
         paused_after_stop_loss=False,
-        place_order_behavior=PlaceOrderBehavior.CHASER,
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
