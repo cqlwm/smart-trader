a1c51fca6d1f62880c3798896fb7693b85cf7b8e
li <weimin.li.dever@gmail.com>
2025-12-12 08:27:41 +0800

    -

--- template/hypeusdt.py
+++ template/hypeusdt.py
@@ -18,14 +18,14 @@ def short_sell(exchange_client: ExSwapClient) -> StrategyTask:
         position_side=PositionSide.SHORT,
         master_side=OrderSide.SELL,
         per_order_qty=2,
-        grid_spacing_rate=0.005,
-        max_order=10,
+        grid_spacing_rate=0.01,
+        max_order=0,
         lowest_price=15,
         enable_exit_signal=True,
         signal=AlphaTrendGridsSignal(AlphaTrendSignal(OrderSide.SELL)),
-        signal_min_take_profit_rate=0.005,
+        signal_min_take_profit_rate=0.01,
         enable_fixed_profit_taking=True,
-        fixed_take_profit_rate=0.01,
+        fixed_take_profit_rate=0.02,
         order_file_path=f'{DATA_PATH}/signal_grid_short_sell_{symbol.simple()}_{timeframe}.json',
     )
     strategy = SignalGridStrategy(config, exchange_client)
