86dfc439534e8acb84940e56e58d10ffecef804c
li <weimin.li.dever@gmail.com>
2026-01-14 21:29:13 +0700

    -

--- template/btcusdc.py
+++ template/btcusdc.py
@@ -28,9 +28,8 @@ def long_buy(exchange_client: ExSwapClient) -> StrategyTask:
         fixed_rate_take_profit=True,
         take_profit_use_limit_order=False,
         fixed_take_profit_rate=0.002,
-        order_file_path=f'{DATA_PATH}/signal_grid_long_buy_{symbol.simple()}_{timeframe}.json',
+        order_file_path=f'{DATA_PATH}/signal_grid_long_buy_position_reverse_{symbol.simple()}_{timeframe}.json',
         position_reverse=True,
-
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
@@ -43,23 +42,24 @@ def short_sell(exchange_client: ExSwapClient) -> StrategyTask:
 
     config=SignalGridStrategyConfig(
         symbol=symbol,
+        timeframe=timeframe,
         position_side=PositionSide.SHORT,
         master_side=OrderSide.SELL,
         per_order_qty=0.002,
-        grid_spacing_rate=0.002,
-        max_order=10,
+        grid_spacing_rate=-0.1,
+        max_order=50,
         enable_exit_signal=True,
-        signal=AlphaTrendSignal(OrderSide.SELL),
-        exit_signal_take_profit_min_rate=-0.1,
+        signal=AlphaTrendGridsSignal(AlphaTrendSignal(OrderSide.BUY)),
+        exit_signal_take_profit_min_rate=0.002,
         fixed_rate_take_profit=True,
-        take_profit_use_limit_order=True,
-        fixed_take_profit_rate=0.0025,
-        order_file_path=f'{DATA_PATH}/signal_grid_short_sell_{symbol.simple()}_{timeframe}.json',
-        enable_max_order_stop_loss=True,
+        take_profit_use_limit_order=False,
+        fixed_take_profit_rate=0.002,
+        order_file_path=f'{DATA_PATH}/signal_grid_short_sell_position_reverse_{symbol.simple()}_{timeframe}.json',
+        position_reverse=True,
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
-    return StrategyTask(symbol=symbol, timeframe=timeframe, strategy=strategy)
+    return StrategyTask(symbol=symbol, strategy=strategy)
 
 def scalping_short(exchange_client: ExSwapClient) -> StrategyTask:
     symbol=Symbol(base="btc", quote="usdc")
