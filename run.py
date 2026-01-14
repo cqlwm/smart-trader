334b0e17e568509121f0d2ea650125d38887a3f4
li <weimin.li.dever@gmail.com>
2026-01-15 06:00:46 +0700

    -

--- run.py
+++ run.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     
     tasks = [
         btcusdc.long_buy(main_binance_client),
-        # hypeusdt.short_sell(copy_trading_binance_client),
+        btcusdc.short_sell(main_binance_client),
     ]
 
     kline_subscribes: List[str] = []
