bd04b884e4cc1c5bf8bdf2887a000bdf80b84e3e
li <weimin.li.dever@gmail.com>
2026-01-16 10:10:22 +0800

    -

--- template/dogeusdc.py
+++ template/dogeusdc.py
@@ -15,6 +15,7 @@ def short_sell(exchange_client: ExSwapClient) -> StrategyTask:
 
     config=SignalGridStrategyConfig(
         symbol=symbol,
+        timeframe=timeframe,
         position_side=PositionSide.SHORT,
         master_side=OrderSide.SELL,
         per_order_qty=200,
@@ -40,21 +41,21 @@ def long_buy(exchange_client: ExSwapClient) -> StrategyTask:
 
     config=SignalGridStrategyConfig(
         symbol=symbol,
+        timeframe=timeframe,
         position_side=PositionSide.LONG,
         master_side=OrderSide.BUY,
-        per_order_qty=200,
+        per_order_qty=50,
         grid_spacing_rate=0.002,
-        max_order=10,
-        # highest_price=0.18852,
+        max_order=50,
         enable_exit_signal=True,
         signal=AlphaTrendGridsSignal(AlphaTrendSignal(OrderSide.BUY)),
-        exit_signal_take_profit_min_rate=0.002,
+        exit_signal_take_profit_min_rate=0.005,
         fixed_rate_take_profit=True,
-        fixed_take_profit_rate=0.015,
-        order_file_path=f'{DATA_PATH}/signal_grid_long_buy_{symbol.simple()}_{timeframe}.json',
-        enable_max_order_stop_loss=True,
+        fixed_take_profit_rate=0.01,
+        order_file_path=f'{DATA_PATH}/signal_grid_long_buy_position_reverse_{symbol.simple()}_{timeframe}.json',
+        position_reverse=True,
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
-    return StrategyTask(symbol=symbol, timeframe=timeframe, strategy=strategy)
+    return StrategyTask(symbol=symbol, strategy=strategy)
 
