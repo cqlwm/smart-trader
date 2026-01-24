6af92f8a2ca78e76ba11ffd45cd98bcc76d2b79b
li <weimin.li.dever@gmail.com>
2026-01-24 19:18:39 +0800

    -

--- template/ethusdc.py
+++ template/ethusdc.py
@@ -25,7 +25,7 @@ def long_buy_position_reverse(exchange_client: ExSwapClient) -> StrategyTask:
         master_side=OrderSide.BUY,
         per_order_qty=0.01,
         grid_spacing_rate=0.0001,
-        max_order=50,
+        max_order=15,
         # highest_price=3188.41,
         # lowest_price=2618.83,
         signal=AlphaTrendGridsSignal(AlphaTrendSignal(OrderSide.BUY)),
@@ -54,34 +54,39 @@ def long_buy_position_reverse(exchange_client: ExSwapClient) -> StrategyTask:
 
     return StrategyTask(symbol=symbol, strategy=strategy)
 
-def short_sell(exchange_client: ExSwapClient) -> StrategyTask:
+def short_sell_position_reverse(exchange_client: ExSwapClient) -> StrategyTask:
     symbol=Symbol(base="eth", quote="usdc")
-    timeframe='15m'
+    timeframe='1m'
 
     config=SignalGridStrategyConfig(
         symbol=symbol,
         timeframe=timeframe,
         position_side=PositionSide.SHORT,
         master_side=OrderSide.SELL,
-        per_order_qty=0.1,
-        grid_spacing_rate=-0.1,
-        max_order=10,
-        signal=AlphaTrendSignal(OrderSide.SELL),
-        order_file_path=f'{DATA_PATH}/signal_grid_short_sell_{symbol.simple()}_{timeframe}.json',
+        per_order_qty=0.01,
+        grid_spacing_rate=0.0001,
+        max_order=15,
+        signal=AlphaTrendGridsSignal(AlphaTrendSignal(OrderSide.SELL)),
+        order_file_path=f'{DATA_PATH}/signal_grid_short_sell_position_reverse_{symbol.simple()}_{timeframe}.json',
 
         enable_exit_signal=True,
-        exit_signal_take_profit_min_rate=-0.02,
+        exit_signal_take_profit_min_rate=0.01,
 
         fixed_rate_take_profit=True,
-        take_profit_use_limit_order=True,
-        fixed_take_profit_rate=0.04,
+        take_profit_use_limit_order=False,
+        fixed_take_profit_rate=0.01,
         
         enable_order_stop_loss=True,
-        order_stop_loss_rate=0.02,
+        order_stop_loss_rate=0.1,
 
-        enable_trailing_stop=True,
-        trailing_stop_rate=0.02,
-        trailing_stop_activation_profit_rate=0.01,
+        # enable_trailing_stop=True,
+        # trailing_stop_rate=0.02,
+        # trailing_stop_activation_profit_rate=0.01,
+
+        enable_max_order_stop_loss=True,
+        paused_after_stop_loss=False,
+
+        position_reverse=True,
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
