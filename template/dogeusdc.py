db774d8b1046b922e0126d1a9021de726ce00213
li <weimin.li.dever@gmail.com>
2026-01-25 11:07:15 +0800

    -

--- template/dogeusdc.py
+++ template/dogeusdc.py
@@ -44,7 +44,7 @@ def long_buy(exchange_client: ExSwapClient) -> StrategyTask:
         timeframe=timeframe,
         position_side=PositionSide.LONG,
         master_side=OrderSide.BUY,
-        per_order_qty=50,
+        per_order_qty=100,
         grid_spacing_rate=0.0001,
         max_order=50,
         enable_exit_signal=True,
@@ -54,6 +54,8 @@ def long_buy(exchange_client: ExSwapClient) -> StrategyTask:
         fixed_take_profit_rate=0.01,
         order_file_path=f'{DATA_PATH}/signal_grid_long_buy_position_reverse_{symbol.simple()}_{timeframe}.json',
         position_reverse=True,
+        enable_max_order_stop_loss=True,
+        
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
