16fd0f51169afbbb18722b5f8f7c0c935709e02a
li <weimin.li.dever@gmail.com>
2026-01-30 15:22:34 +0800

    -

--- run.py
+++ run.py
@@ -35,16 +35,17 @@ main_binance_client: BinanceSwapClient = create_binance_client('main')
 
 if __name__ == '__main__':
 
-    from template import dogeusdc, ethusdc, fogousdt, litusdt, nightusdt, xnyusdt, solusdc
+    from template import xauusdt
     
     tasks = [
+        xauusdt.template(main_binance_client),
         # ethusdc.short_sell_position_reverse(main_binance_client),
-        dogeusdc.long_buy_position_reverse(main_binance_client),
+        # dogeusdc.long_buy_position_reverse(main_binance_client),
         # fogousdt.long_buy(main_binance_client),
         # nightusdt.long_buy_position_reverse(main_binance_client),
         # xnyusdt.long_buy_position_reverse(main_binance_client),
 
-        litusdt.long_buy_position_reverse(main_binance_client),
+        # litusdt.long_buy_position_reverse(main_binance_client),
         # litusdt.short_sell_position_reverse(main_binance_client),
 
     ]
---
+++ template/xauusdt.py
@@ -0,0 +1,30 @@
+from client.ex_client import ExSwapClient
+import log
+from model import OrderSide, PositionSide, Symbol
+from task.strategy_task import StrategyTask
+from strategy.simple_grid_strategy_v2 import (
+    SimpleGridStrategy,
+    SimpleGridStrategyConfig,
+)
+from template.template_model import TemplateModel
+
+logger = log.getLogger(__name__)
+
+
+def template(exchange_client: ExSwapClient) -> StrategyTask:
+    """
+    简单网格策略模板
+    """
+    symbol = Symbol(base="XAU", quote="USDT")
+    config = SimpleGridStrategyConfig(
+        symbol=symbol,
+        upper_price=5000,
+        lower_price=4000,
+        grid_num=10,
+        quantity_per_grid=0.002,
+        position_side=PositionSide.LONG,
+        master_order_side=OrderSide.BUY,
+        active_grid_count=8,
+    )
+    simple_grid_strategy = SimpleGridStrategy(ex_client=exchange_client, config=config, timeframe="1m")
+    return StrategyTask(symbol=symbol, strategy=simple_grid_strategy)
