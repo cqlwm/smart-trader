152a384af02b43a75a4b95c95019f9b992786976
li <weimin.li.dever@gmail.com>
2026-02-26 08:32:28 +0800

    -

--- run.py
+++ run.py
@@ -35,10 +35,11 @@ main_binance_client: BinanceSwapClient = create_binance_client('main')
 
 if __name__ == '__main__':
 
-    from template import dogeusdc
+    from template import dogeusdc, ethusdc
     
     tasks = [
         dogeusdc.long_buy(main_binance_client),
+        ethusdc.long_buy(main_binance_client),
     ]
 
     kline_subscribes: List[str] = []
--- template/ethusdc.py
+++ template/ethusdc.py
@@ -4,12 +4,35 @@ from model import OrderSide, PositionSide, Symbol
 from strategy.grids_strategy_v2 import SignalGridStrategy, SignalGridStrategyConfig
 from strategy.alpha_trend_signal.alpha_trend_signal import AlphaTrendSignal
 from strategy.alpha_trend_signal.alpha_trend_grids_signal import AlphaTrendGridsSignal
-from strategy.scalping_strategy import ScalpingStrategy, ScalpingStrategyConfig
-from strategy.alpha_trend_strategy import AlphaTrendStrategy, AlphaTrendStrategyConfig
 from config import DATA_PATH
 from task.strategy_task import StrategyTask
 
 logger = log.getLogger(__name__)
+
+_symbol=Symbol(base="eth", quote="usdc")
+
+def long_buy(exchange_client: ExSwapClient) -> StrategyTask:
+    timeframe='5m'
+
+    config=SignalGridStrategyConfig(
+        symbol=_symbol,
+        timeframe=timeframe,
+        position_side=PositionSide.LONG,
+        master_side=OrderSide.BUY,
+        per_order_qty=0.04,
+        grid_spacing_rate=-0.1,
+        max_order=10,
+        enable_exit_signal=True,
+        signal=AlphaTrendGridsSignal(AlphaTrendSignal(OrderSide.BUY)),
+        exit_signal_take_profit_min_rate=0.1,
+        fixed_rate_take_profit=True,
+        fixed_take_profit_rate=0.1,
+        order_file_path=f'{DATA_PATH}/signal_grid_long_buy_{_symbol.simple()}_{timeframe}.json',
+        enable_max_order_stop_loss=True,
+    )
+    strategy = SignalGridStrategy(config, exchange_client)
+
+    return StrategyTask(symbol=_symbol, strategy=strategy)
 
 def long_buy_position_reverse(exchange_client: ExSwapClient) -> StrategyTask:
     symbol=Symbol(base="eth", quote="usdc")
