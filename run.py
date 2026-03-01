56df84c2b941d4b2535d462ddfa219b97e7eda53
li <weimin.li.dever@gmail.com>
2026-03-01 11:05:03 +0800

    -

--- run.py
+++ run.py
@@ -38,8 +38,8 @@ if __name__ == '__main__':
     from template import dogeusdc, xauusdt
     
     tasks = [
-        # dogeusdc.long_buy(main_binance_client),
-        xauusdt.long_buy(main_binance_client),
+        dogeusdc.long_buy(main_binance_client),
+        # xauusdt.long_buy(main_binance_client),
     ]
 
     kline_subscribes: List[str] = []
