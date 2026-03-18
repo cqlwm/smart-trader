2da97adf71e3e533da5995d12e1e91116cb2c3f4
li <weimin.li.dever@gmail.com>
2026-03-19 00:16:25 +0800

    -

--- template/dogeusdc.py
+++ template/dogeusdc.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from client.ex_client import ExSwapClient
 import log
 from model import OrderSide, PositionSide, Symbol
@@ -9,13 +11,14 @@ from task.strategy_task import StrategyTask
 
 logger = log.getLogger(__name__)
 
-def short_sell_position_reverse(exchange_client: ExSwapClient) -> StrategyTask:
-    symbol=Symbol(base="doge", quote="usdc")
-    timeframe='1m'
+symbol_=Symbol(base="doge", quote="usdc")
+timeframe_= '5m'
+today_utc = datetime.now(timezone.utc).strftime("%Y%m%d")
 
+def short_sell_position_reverse(exchange_client: ExSwapClient) -> StrategyTask:
     config=SignalGridStrategyConfig(
-        symbol=symbol,
-        timeframe=timeframe,
+        symbol=symbol_,
+        timeframe=timeframe_,
         position_side=PositionSide.SHORT,
         master_side=OrderSide.SELL,
         per_order_qty=200,
@@ -26,22 +29,19 @@ def short_sell_position_reverse(exchange_client: ExSwapClient) -> StrategyTask:
         exit_signal_take_profit_min_rate=0.005,
         fixed_rate_take_profit=True,
         fixed_take_profit_rate=0.01,
-        order_file_path=f'{DATA_PATH}/signal_grid_short_sell_position_reverse_{symbol.simple()}_{timeframe}.json',
+        order_file_path=f'{DATA_PATH}/signal_grid_short_sell_position_reverse_{symbol_.simple()}_{timeframe_}.json',
         enable_max_order_stop_loss=True,
         paused_after_stop_loss=False,
         position_reverse=True,
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
-    return StrategyTask(symbol=symbol, strategy=strategy)
+    return StrategyTask(symbol=symbol_, strategy=strategy)
 
 def long_buy_position_reverse(exchange_client: ExSwapClient) -> StrategyTask:
-    symbol=Symbol(base="doge", quote="usdc")
-    timeframe='1m'
-
     config=SignalGridStrategyConfig(
-        symbol=symbol,
-        timeframe=timeframe,
+        symbol=symbol_,
+        timeframe=timeframe_,
         position_side=PositionSide.LONG,
         master_side=OrderSide.BUY,
         per_order_qty=200,
@@ -52,21 +52,18 @@ def long_buy_position_reverse(exchange_client: ExSwapClient) -> StrategyTask:
         exit_signal_take_profit_min_rate=0.005,
         fixed_rate_take_profit=True,
         fixed_take_profit_rate=0.01,
-        order_file_path=f'{DATA_PATH}/signal_grid_long_buy_position_reverse_{symbol.simple()}_{timeframe}.json',
+        order_file_path=f'{DATA_PATH}/signal_grid_long_buy_position_reverse_{symbol_.simple()}_{timeframe_}.json',
         position_reverse=True,
         enable_max_order_stop_loss=True,
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
-    return StrategyTask(symbol=symbol, strategy=strategy)
-
-symbol=Symbol(base="doge", quote="usdc")
-timeframe='5m'
+    return StrategyTask(symbol=symbol_, strategy=strategy)
 
 def long_buy(exchange_client: ExSwapClient) -> StrategyTask:
     config=SignalGridStrategyConfig(
-        symbol=symbol,
-        timeframe=timeframe,
+        symbol=symbol_,
+        timeframe=timeframe_,
         position_side=PositionSide.LONG,
         master_side=OrderSide.BUY,
         per_order_qty=3500,
@@ -77,16 +74,16 @@ def long_buy(exchange_client: ExSwapClient) -> StrategyTask:
         exit_signal_take_profit_min_rate=0.03,
         fixed_rate_take_profit=True,
         fixed_take_profit_rate=0.03,
-        order_file_path=f'{DATA_PATH}/signal_grid_long_buy_{symbol.simple()}_{timeframe}.json',
+        order_file_path=f'{DATA_PATH}/signal_grid_long_buy_{symbol_.simple()}_{timeframe_}_{today_utc}.json',
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
-    return StrategyTask(symbol=symbol, strategy=strategy)
+    return StrategyTask(symbol=symbol_, strategy=strategy)
 
 def short_sell(exchange_client: ExSwapClient) -> StrategyTask:
     config=SignalGridStrategyConfig(
-        symbol=symbol,
-        timeframe=timeframe,
+        symbol=symbol_,
+        timeframe=timeframe_,
         position_side=PositionSide.SHORT,
         master_side=OrderSide.SELL,
         per_order_qty=3500,
@@ -97,11 +94,11 @@ def short_sell(exchange_client: ExSwapClient) -> StrategyTask:
         exit_signal_take_profit_min_rate=0.03,
         fixed_rate_take_profit=True,
         fixed_take_profit_rate=0.03,
-        order_file_path=f'{DATA_PATH}/signal_grid_short_sell_{symbol.simple()}_{timeframe}.json',
+        order_file_path=f'{DATA_PATH}/signal_grid_short_sell_{symbol_.simple()}_{timeframe_}_{today_utc}.json',
     )
     strategy = SignalGridStrategy(config, exchange_client)
 
-    return StrategyTask(symbol=symbol, strategy=strategy)
+    return StrategyTask(symbol=symbol_, strategy=strategy)
 
 
 def market_trend_task(exchange_client: ExSwapClient) -> StrategyTask | None:
@@ -112,8 +109,8 @@ def market_trend_task(exchange_client: ExSwapClient) -> StrategyTask | None:
         Symbol(base="doge", quote="usdc"),
     ]
 
-    def prev_day_change(symbol: Symbol) -> float:
-        ohlcv = exchange_client.fetch_ohlcv(symbol, '1d', limit=2)
+    def prev_day_change(s: Symbol) -> float:
+        ohlcv = exchange_client.fetch_ohlcv(s, '1d', limit=2)
         prev = ohlcv[-2]
         return prev.close - prev.open
 
