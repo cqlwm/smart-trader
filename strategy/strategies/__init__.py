51f5cc7fe8a76a045bbcdcb65647353c4bf059d5
li <weimin.li.dever@gmail.com>
2026-05-05 17:32:00 +0800

    feat: add strategy auto-discovery module

---
+++ strategy/strategies/__init__.py
@@ -0,0 +1,7 @@
+from strategy.signal_grid_strategy import SignalGridStrategy  # noqa: F401
+from strategy.daily_trend_strategy import DailyTrendStrategy  # noqa: F401
+from strategy.smc_signal.smc_intraday_strategy import SMCIntradayStrategy  # noqa: F401
+from strategy.simple_grid_strategy import SimpleGridStrategy  # noqa: F401
+
+from strategy.alpha_trend_signal.alpha_trend_signal import AlphaTrendSignal  # noqa: F401
+from strategy.alpha_trend_signal.alpha_trend_grids_signal import AlphaTrendGridsSignal  # noqa: F401
